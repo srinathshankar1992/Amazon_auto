33df564b4878170e78f4e4d08226f4d99bbb7378
srinathshankar1992 <srinathshankards@gmail.com>
2021-02-16 23:22:16 +0530

    thagonkn

---
+++ src/main/java/Resource/RetryRepeatedTestCases.java
@@ -0,0 +1,19 @@
+package Resource;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+public class RetryRepeatedTestCases implements IAnnotationTransformer {
+
+	public void transform(ITestAnnotation annotation, Class arg1, Constructor arg2, Method arg3) {
+		
+		annotation.setRetryAnalyzer(RetryAnalyzer.class);
+		
+	}
+
+	
+	
+}
---
+++ src/test/java/Amazon/Automate/retrytest.java
@@ -0,0 +1,19 @@
+package Amazon.Automate;
+
+import org.testng.annotations.Test;
+
+public class retrytest {
+
+	@Test
+	public void abc()
+	{
+	org.testng.Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void bbc()
+	{
+		org.testng.Assert.assertEquals(false, true);
+	}
+	
+}
